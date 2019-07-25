d15cfcf7c1754ae468b1d82218efec045f3f8213
Tony Li <litianxiao1121@gmail.com>
2019-07-25 16:08:47 -0400

    created Java Projected

---
+++ JavaCodes/src/easy/DefangingIPAddress.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class DefangingIPAddress {
+	/*
+	 * Time: 10mins
+	 * Date: 2019-07-25
+	 * Time Complexity: O(N)
+	 */
+	public String defangIPaddr(String address) {
+		if (address == null || address.length() == 0)
+			return address;
+		final int length = address.length();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < length; i++) {
+			char c = address.charAt(i);
+			if (c == '.')
+				sb.append("[.]");
+			else
+				sb.append(c);
+		}
+		return sb.toString();
+	}
+}
